76e67e24c827daaec28fd94ac2b36a725da657b5
Kostya Deev <kostya.deev@bluware.com>
2021-10-29 09:42:35 -0500

    sort tfrecord files

--- test_tfrecord.py
+++ test_tfrecord.py
@@ -1,15 +1,17 @@
 import tensorflow as tf
 import os
 
-tfrecord_path = "/mnt/Data/imagenet-object-localization-challenge/tfrecord"
+tfrecord_path = "/_"
 
 files = [os.path.join(tfrecord_path, f) for f in os.listdir(tfrecord_path) if os.path.isfile(os.path.join(tfrecord_path, f))]
-
+files = sorted(files)
 dataset = tf.data.TFRecordDataset(files)
 
-for raw_record in dataset.take(10):
+for raw_record in dataset:
   example = tf.train.Example()
   example.ParseFromString(raw_record.numpy())
+  height = example.features.feature["image/height"].int64_list.value[0]
+  width = example.features.feature["image/width"].int64_list.value[0]
   filename = example.features.feature["image/filename"].bytes_list.value[0].decode("utf-8")
   image = example.features.feature["image/encoded"].bytes_list.value[0]
 #   image = np.frombuffer(image)
